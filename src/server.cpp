178839935f77f8ea5faaf5963795f015239a6334
Steven Mead <steven.mead23@gmail.com>
2024-03-27 16:28:31 -0500

    pass 5th stage

--- src/server.cpp
+++ src/server.cpp
@@ -28,6 +28,7 @@ int clientHandler(int connfd) {
         send_buff += std::to_string(pos2-pos1) + "\n\n";
         send_buff += receive.substr(pos1, pos2-pos1) + "\r\n\r\n";
     } else if (receive.find("/user-agent") != std::string::npos) {
+        std::cout << receive << std::endl;
         send_buff = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: ";
         std::string::size_type pos1 = receive.rfind("User-Agent") + 12;
         std::string::size_type pos2 = receive.find("\n", pos1);
