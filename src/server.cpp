875c574a282e9b614f87b650f3f7a5afc61411a6
Steven Mead <steven.mead23@gmail.com>
2024-03-27 16:33:02 -0500

    pass 5th stage

--- src/server.cpp
+++ src/server.cpp
@@ -33,6 +33,7 @@ int clientHandler(int connfd) {
         std::string::size_type pos1 = receive.rfind("User-Agent") + 12;
         std::string::size_type pos2 = receive.find("\n", pos1);
         send_buff += std::to_string(pos2-pos1) + "\n\n";
+        std::cout << "Grabbed line: " << receive.substr(pos1, pos2-pos1) << std::endl;
         send_buff += receive.substr(pos1, pos2-pos1) + "\r\n\r\n";
     } else {
         send_buff = "HTTP/1.1 404 Not Found\r\n\r\n";
