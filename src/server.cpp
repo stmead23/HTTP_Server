877b28a67fcfb96eac1f7d8303ba6a6130abdef5
Steven Mead <steven.mead23@gmail.com>
2024-03-30 13:22:46 -0500

    pass 5th stage

--- src/server.cpp
+++ src/server.cpp
@@ -89,7 +89,7 @@ int main(int argc, char **argv) {
     std::vector<std::thread> client_pool;
     while (true) {
         int connfd = accept(server_fd, NULL, NULL);
-        client_pool.emplace_back(std::thread(clientHandler(connfd)));
+        client_pool.emplace_back(std::thread(clientHandler, connfd));
     }
     
     for (auto &x : client_pool) {
